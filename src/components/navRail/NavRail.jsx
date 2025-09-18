3f91ca64f286121c78aae4ee5e670fea28ef8eba
William Wang <william1997au@gmail.com>
2025-09-18 22:10:03 +1000

    feat(frontend): add SVG icons, enable SVGR, and update NavRail

--- src/components/navRail/NavRail.jsx
+++ src/components/navRail/NavRail.jsx
@@ -1,7 +1,16 @@
 import styles from "./NavRail.module.css";
+import ChatIcon from "../../assets/chat.svg?react";
+import UserIcon from "../../assets/user.svg?react";
+import AddUserIcon from "../../assets/addUser.svg?react";
 
 const NavRail = () => {
-  return <div className={styles.navRail}>NavRail</div>;
+  return (
+    <div className={styles.navRail}>
+      <UserIcon className={styles.navRailItem} />
+      <ChatIcon className={styles.navRailItem} />
+      <AddUserIcon className={styles.navRailItem} />
+    </div>
+  );
 };
 
 export default NavRail;
